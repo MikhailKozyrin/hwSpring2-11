d96fbb0344b03fa5d5da67d3bdaf0398ef2a655f
user <kozyan94@gmail.com>
2022-10-25 22:06:06 +0300

    hw 2.11

---
+++ src/main/java/pro/sky/hwspring/HwSpring211Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hwspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpring211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpring211Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hwspring/HwSpring211ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hwspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpring211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
